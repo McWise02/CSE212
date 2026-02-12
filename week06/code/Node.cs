41435231a298c994f6c0be4cf67770b729a53b47
McWise02 <micah02.brown@gmail.com>
2026-02-12 21:31:37 +0100

    Finished

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -3,6 +3,7 @@ public class Node
     public int Data { get; set; }
     public Node? Right { get; private set; }
     public Node? Left { get; private set; }
+    public Node? Parent { get; private set;}
 
     public Node(int data)
     {
@@ -11,35 +12,55 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == Data) return; // no duplicates
 
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
+            {
                 Left = new Node(value);
+                Left.Parent = this;
+            }
             else
+            {
                 Left.Insert(value);
+            }
         }
         else
         {
-            // Insert to the right
             if (Right is null)
+            {
                 Right = new Node(value);
+                Right.Parent = this;
+            }
             else
+            {
                 Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+        else if (value < Data)
+        {
+            if (Left is null) return false;
+            else return Left.Contains(value);
+        }
+
+        if (Right is null) return false;
+        else return Right.Contains(value);
+    
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
